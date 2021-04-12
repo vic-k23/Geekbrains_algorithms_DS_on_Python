d9081827fceab4482c3fcc6f7109bd7c26c4cdcc
Victor Kozlov <vick23@mail.ru>
2021-04-12 10:32:22 +0300

    [fix] Исправлено решение по 8-му заданию. Считал столбцы, а надо было строки

--- Lesson3/hw08.py
+++ Lesson3/hw08.py
@@ -2,15 +2,17 @@
 # Программа должна вычислять сумму введенных элементов каждой строки и записывать ее в последнюю ячейку строки.
 # В конце следует вывести полученную матрицу.
 
-m = [0] * 5
-m[4] = [0] * 4
-for i in range(4):
-    m[i] = []
-    for j in range(4):
-        m[i].append(int(input(f"Введите элемент {j} строки {i}: ")))
-        m[4][j] += m[i][j]
 
-for lst in m:
-    for itm in lst:
-        print(f"{itm:>4}", end='')
+m = []
+for row in range(5):
+    m.append([])
+    summ = 0
+    for col in range(3):
+        m[row].append(int(input(f"Введите элемент {col} строки {row}: ")))
+        summ += m[row][col]
+    m[row].append(summ)
+
+for row in m:
+    for itm in row:
+        print(f"{itm:>5}", end='')
     print()
